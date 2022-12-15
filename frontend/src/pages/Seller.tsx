c65f39a24c56c83f889c337757046d8d632ba83f
Adetunji Oyebanji <adetunji.oyebanji@andela.com>
2022-12-15 14:51:49 +0100

    Only buyers can buy

--- frontend/src/pages/Seller.tsx
+++ frontend/src/pages/Seller.tsx
@@ -1,7 +1,7 @@
 import { Button, Col, Form, InputNumber, message, Modal, Row, Table, TableColumnsType } from "antd";
 import { useEffect, useMemo, useState } from "react";
 import RestService from "../services/RestService";
-import { AppActionType, AppComponentProps, AuthUser, LaravelPagination, Product } from "../types";
+import { AppActionType, AppComponentProps, AuthUser, LaravelPagination, Product, RoleName } from "../types";
 
 export default function Seller({ state, dispatch }: AppComponentProps) {
     const [dataSource, setDataSource] = useState<LaravelPagination<Product[]>>();
@@ -32,84 +32,88 @@ export default function Seller({ state, dispatch }: AppComponentProps) {
             key: "amountAvailable"
         },{
             title: "",
-            key: "amountAvailable",
+            key: "buy",
             render: (value, record) => {
                 return (
-                    <Button
-                        onClick={() => {
-                            Modal.confirm({
-                                title: `Buy ${record.productName}`,
-                                content: (
-                                    <>
-                                        <Form
-                                            form={form}
-                                        >
-                                            <Form.Item
-                                                name={"amount"}
-                                                label={"Amount"}
-                                                rules={[
-                                                    {
-                                                        required: true,
-                                                        max: record.amountAvailable,
-                                                        min: 1,
-                                                        type: 'number'
-                                                    }
-                                                ]}
+                    <>
+                    { state?.user?.roleName === RoleName.BUYER &&
+                        <Button
+                            onClick={() => {
+                                Modal.confirm({
+                                    title: `Buy ${record.productName}`,
+                                    content: (
+                                        <>
+                                            <Form
+                                                form={form}
                                             >
-                                                <InputNumber
-                                                    placeholder="Enter the amount you want to buy"
-                                                />
-                                            </Form.Item>
-                                        </Form>
-                                        {/* <div>
-                                            Total: { total }
-                                        </div> */}
-                                    </>
-
-                                ),
-                                onOk() {
-                                    form.submit();
-                                    return form.validateFields().then(values => {
-                                        client.buyProduct(record.id, values.amount)
-                                        .then(resp => {
-                                            message.success(`Successfully bought ${record.productName}`);
-                                            if(dataSource && dataSource.data) {
-                                                const newData: Product[] = dataSource.data.map(d => {
-                                                    if(d.id === record.id) {
-                                                        return {
-                                                            ...d,
-                                                            amountAvailable: d.amountAvailable - values.amount
+                                                <Form.Item
+                                                    name={"amount"}
+                                                    label={"Amount"}
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            max: record.amountAvailable,
+                                                            min: 1,
+                                                            type: 'number'
                                                         }
-                                                    }
-                                                    return d;
-                                                });
-                                                setDataSource({
-                                                    ...dataSource,
-                                                    data: newData
-                                                });
-                                            }
-                                            dispatch({
-                                                type: AppActionType.STORE_USER,
-                                                payload: {
-                                                    ...state?.user,
-                                                    deposit: resp.change
-                                                } as AuthUser
-                                            })
-                                            return resp;
-                                        }).catch(e => {
-                                            message.error(e.message || e.error);
-                                        })
-                                    })
-                                },
-                                onCancel() {
-                                    form.resetFields();
-                                },
-                            })
+                                                    ]}
+                                                >
+                                                    <InputNumber
+                                                        placeholder="Enter the amount you want to buy"
+                                                    />
+                                                </Form.Item>
+                                            </Form>
+                                            {/* <div>
+                                                Total: { total }
+                                            </div> */}
+                                        </>
 
-                        }}
-                    >
+                                    ),
+                                    onOk() {
+                                        form.submit();
+                                        return form.validateFields().then(values => {
+                                            client.buyProduct(record.id, values.amount)
+                                            .then(resp => {
+                                                message.success(`Successfully bought ${record.productName}`);
+                                                if(dataSource && dataSource.data) {
+                                                    const newData: Product[] = dataSource.data.map(d => {
+                                                        if(d.id === record.id) {
+                                                            return {
+                                                                ...d,
+                                                                amountAvailable: d.amountAvailable - values.amount
+                                                            }
+                                                        }
+                                                        return d;
+                                                    });
+                                                    setDataSource({
+                                                        ...dataSource,
+                                                        data: newData
+                                                    });
+                                                }
+                                                dispatch({
+                                                    type: AppActionType.STORE_USER,
+                                                    payload: {
+                                                        ...state?.user,
+                                                        deposit: resp.change
+                                                    } as AuthUser
+                                                })
+                                                return resp;
+                                            }).catch(e => {
+                                                message.error(e.message || e.error);
+                                            })
+                                        })
+                                    },
+                                    onCancel() {
+                                        form.resetFields();
+                                    },
+                                })
+
+                            }}
+                        >
                         Buy
-                    </Button>
+                        </Button>
+                    }
+                    </>
                 )
             }
         }
